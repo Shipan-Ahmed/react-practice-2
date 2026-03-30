e4bfed207d87229d4fa1e7055bf63b79fd20a7c5
Shipan Ahmed <shipan.cse.brur@gmail.com>
2026-03-31 02:47:34 +0600

    added card

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -5,14 +5,15 @@ const PricingCard = ({ pricing }) => {
     const { name, price,  features } = pricing;
     console.log(pricing);
     return (
-      <div className="bg-yellow-200 border rounded-2xl p-4">
+      <div className="bg-yellow-200 border rounded-2xl p-4 flex flex-col">
         <h3 className="text-4xl">{name}</h3>
         <p className="text-2xl">{price}</p>
-        <div className='mt-8 bg-amber-400 rounded-2xl p-4 text-black '>
-                {
-                    features.map((feature, index)=> <CardFeature key={index} feature={feature}></CardFeature>)
-                }
+        <div className="mt-8 bg-amber-400 rounded-2xl p-4 text-black flex-1 ">
+          {features.map((feature, index) => (
+            <CardFeature key={index} feature={feature}></CardFeature>
+          ))}
         </div>
+        <button className="btn btn-accent w-full mt-4">Subscribe</button>
       </div>
     );
 };
--- src/Components/PricingOptions/PricingOptions.jsx
+++ src/Components/PricingOptions/PricingOptions.jsx
@@ -1,6 +1,7 @@
 import { ProjectorIcon } from 'lucide-react';
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyCard from '../DaisyCard/DaisyCard';
 
 const PricingOptions = ({ pricingPromises }) => {
     const pricingData = use(pricingPromises);
@@ -8,11 +9,14 @@ const PricingOptions = ({ pricingPromises }) => {
     return (
       <div>
         <h2 className='text-6xl text-center mt-8'>Get our membership</h2>
-        <div className='grid md:grid-cols-3 gap-4 p-4'>
-                {
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+                {/* {
                     pricingData.pricing.map((pricing) => (
                     <PricingCard key={pricing.id} pricing={pricing}></PricingCard>
-                ))}
+                ))} */}
+                {
+                    pricingData.pricing.map(pricing=> <DaisyCard key={pricing.id} pricing={pricing}></DaisyCard>)
+                }
         </div>
       </div>
     );
